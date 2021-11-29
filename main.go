ef5249f41f408dbd05c942be3722866100b557b9
saikirand007 <saikiran986d@gmail.com>
2021-11-30 02:07:19 +0530

    added a new database

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/Saikirand007/Backend_of_hosipital_app/database"
 	"github.com/Saikirand007/Backend_of_hosipital_app/hospital"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 
 	"github.com/gofiber/fiber"
 )
@@ -21,10 +26,24 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/v1/hospital", hospital.DeleteOldBookingIds)
 }
 
+func initDataBase() {
+
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "hosipitalRecod.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+
+}
+
 func main() {
 	app := fiber.New()
+	initDataBase()
 
 	setupRoutes(app)
 
 	app.Listen(":3000")
+
+	defer database.DBConn.Close()
 }
